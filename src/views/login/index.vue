923429a96793feff4a7f3fc6346a7d1520f3400c
fixl <huanxianglong@tiangongwang.com>
2024-02-26 14:06:42 +0800

    .

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,7 +5,7 @@
       <img src="/public/ye.svg" class="w-[48px]" alt="">
         <el-input class="mt-[24px]" placeholder="请输入账号" />
         <el-input class="mt-[24px]" placeholder="请输入密码" type="password" />
-        <el-button :type="isSingUp?'success':'primary'" class="w-full mt-[24px]">{{isSingUp?'注册':'登录'}}</el-button>
+        <el-button :type="isSingUp?'primary':'primary'" class="w-full mt-[24px]">{{isSingUp?'注册':'登录'}}</el-button>
         <span class="text-[var(--primary)] self-end mt-[12px] cursor-pointer" @click="isSingUp=!isSingUp">{{isSingUp?'返回登录':'去注册'}}</span>
     </div>
   </div>
@@ -38,5 +38,8 @@ const isSingUp = ref(false)
   box-shadow: 1px 1px 6px 0 #bfbfbf4d;
   border-radius: 10px;
   // transition:all .5s;
+  :deep(.el-input__wrapper){
+    background-color: #f8f8f8;
+  }
 }
 </style>
